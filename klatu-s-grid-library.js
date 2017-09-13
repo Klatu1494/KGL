80f00fc300db02dc16d41d219c8e875eb6140ead
Klatu1494 <klatu1494@gmail.com>
2017-09-13 08:16:58 -0300

    My plans for the move method were commented.

--- klatu-s-grid-library.js
+++ klatu-s-grid-library.js
@@ -32,7 +32,7 @@ class KGLGrid {
           if (x) {
             currentTile.linkWith(this.tiles[x - 1][y]);
           }
-          if (y && (this.tileSides != 3 || x % 2)) {
+          if (y && (this.tileSides != 3 || (x % 2 == 1) && (y % 2 == 0) || (y % 2 == 1) && (x % 2 == 0))) {
             currentTile.linkWith(this.tiles[x][y - 1])
           }
           if (this.tileSides === 6) {
@@ -59,7 +59,37 @@ class KGLGrid {
   }
 
   move(unit, tile) {
+    var currentTile = this.tiles[unit.position.x][unit.position.y]
+    var openTiles = [];
+    var closedTiles = [];
+    for (var row of this.tiles)
+      for (var tile of row) {
+        tile.minCost = null;
+        tile.parent = null;
+        tile.f = null;
+      }
+    currentTile.f = 0;
+    /*
+    TO DO:
+    while the open list is not empty
+      find the node with the least f on the open list, call it "q"
+      pop q off the open list
+      generate q 's 8 successors and set their parents to q
+      for each successor
+        if successor is the goal, stop the search
+        successor.g = q.g + distance between successor and q
+        successor.h = distance from goal to successor
+        successor.f = successor.g + successor.h
 
+        if a node with the same position as successor is in the OPEN list\
+        which has a lower f than successor, skip this successor
+        if a node with the same position as successor is in the CLOSED list\
+        which has a lower f than successor, skip this successor
+        otherwise, add the node to the open list
+      end
+      push q on the closed list
+    end
+    */
   }
 }
 
